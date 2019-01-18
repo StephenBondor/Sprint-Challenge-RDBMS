bab23c237ab59a859226ad55ac3ca8118414472b
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2019-01-18 09:44:39 -0800

    Add one stretch: CRUD

--- index.js
+++ index.js
@@ -60,10 +60,35 @@ server.get('/api/:table/:id', (req, res) => {
 	}
 });
 
+//list the entire table
 server.get('/api/:table', (req, res) => {
 	db(req.params.table)
 		.then(things => {
 			res.status(200).json(things);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+// delete whatever
+server.delete('/api/:table/:id', (req, res) => {
+	const id = req.params.id;
+	db(req.params.table)
+		.where({id: id})
+		.del()
+		.then(thing => {
+			res.status(200).json(thing);
+		})
+		.catch(err => res.status(500).json(err));
+});
+
+// update whatever
+server.put('/api/:table/:id', (req, res) => {
+	const id = req.params.id;
+	db(req.params.table)
+		.where({id: id})
+		.update(req.body)
+		.then(thing => {
+			res.status(200).json(thing);
 		})
 		.catch(err => res.status(500).json(err));
 });
